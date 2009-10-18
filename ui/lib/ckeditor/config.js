2392a1ba8d7dd9d3c04d2c6375113fc53fc5affe
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2009-10-18 16:09:25 +0800

    Fix backend error.

---
+++ ui/lib/ckeditor/config.js
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+    [
+        ['NewPage','Preview'],
+        ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+        '/',
+        ['Styles','Format'],
+        ['Bold','Italic','Strike'],
+        ['Link','Unlink','Anchor'],
+        ['Maximize','-','About']
+    ];
+};
+
